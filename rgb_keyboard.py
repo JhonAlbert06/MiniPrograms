354b3f9cdf0214bd8bc55cd453d09bdb3c8f3bf6
“Jhon”Albert06 <Jhonalbertguzmanrosario@gmail.com>
2024-12-09 15:49:27 -0400

    eliminacion de archivos

--- rgb_keyboard.py
+++
@@ -1,7 +0,0 @@
-from pyrgb import RGBColor, DeviceManager
-
-manager = DeviceManager()
-keyboard = manager.get_device('keyboard')  # Ajusta según tu dispositivo
-
-color = RGBColor(255, 182, 193)  # Rosado claro
-keyboard.set_static(color)
