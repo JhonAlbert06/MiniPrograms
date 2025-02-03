a38f12992aa5eb1b3f01dd1421ff11ecd318fc40
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2025-02-03 10:09:37 -0400

    febrero 02 2025

--- progreso_gym.py
+++ progreso_gym.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 # Datos
 datos = [
-    {"fecha": "02 de Enero - 2024", 
+    {"fecha": "Enero - 2024", 
      "peso": 225, 
      "brazo": {"izquierdo": 13.5, "derecho": 13}, 
      "cintura": {"min": 38, "max": 41.3}, 
@@ -10,7 +10,7 @@ datos = [
      "pantorrillas": {"izquierda": 17.1, "derecha": 17}, 
      "pierna": {"izquierda": 27, "derecha": 26.3}},
     
-    {"fecha": "02 de Febrero - 2024", 
+    {"fecha": "Febrero - 2024", 
      "peso": 225.6, 
      "brazo": {"izquierdo": 15, "derecho": 14}, 
      "cintura": {"min": 37.1, "max": 39.7}, 
@@ -18,7 +18,7 @@ datos = [
      "pantorrillas": {"izquierda": 17.3, "derecha": 17.3}, 
      "pierna": {"izquierda": 27.2, "derecha": 27}},
     
-    {"fecha": "02 de Marzo - 2024", 
+    {"fecha": "Marzo - 2024", 
      "peso": 219.5, 
      "brazo": {"izquierdo": 15, "derecho": 14.4}, 
      "cintura": {"min": 39.6, "max": 40}, 
@@ -26,7 +26,7 @@ datos = [
      "pantorrillas": {"izquierda": 16.6, "derecha": 17}, 
      "pierna": {"izquierda": 27.3, "derecha": 27.3}},
     
-    {"fecha": "02 de Abril - 2024", 
+    {"fecha": "Abril - 2024", 
      "peso": 216.3, 
      "brazo": {"izquierdo": 14.1, "derecho": 14.3}, 
      "cintura": {"min": 35.3, "max": 39}, 
@@ -34,7 +34,7 @@ datos = [
      "pantorrillas": {"izquierda": 16.4, "derecha": 16.5}, 
      "pierna": {"izquierda": 27, "derecha": 27}},
     
-    {"fecha": "02 de Mayo - 2024", 
+    {"fecha": "Mayo - 2024", 
      "peso": 218.8, 
      "brazo": {"izquierdo": 14.1, "derecho": 14.3}, 
      "cintura": {"min": 35.3, "max": 38}, 
@@ -42,7 +42,7 @@ datos = [
      "pantorrillas": {"izquierda": 16.5, "derecha": 16.6}, 
      "pierna": {"izquierda": 27, "derecha": 26.9}},
     
-    {"fecha": "02 de Junio - 2024", 
+    {"fecha": "Junio - 2024", 
      "peso": 214.5, 
      "brazo": {"izquierdo": 14.4, "derecho": 14.0}, 
      "cintura": {"min": 34.5, "max": 37.4}, 
@@ -51,7 +51,7 @@ datos = [
      "pierna": {"izquierda": 26.6, "derecha": 26.1}, 
      "pecho": 41},
     
-    {"fecha": "02 de Julio - 2024", 
+    {"fecha": "Julio - 2024", 
      "peso": 213.2, 
      "brazo": {"izquierdo": 14, "derecho": 13.7}, 
      "cintura": {"min": 34, "max": 37}, 
@@ -60,7 +60,7 @@ datos = [
      "pierna": {"izquierda": 26, "derecha": 26.2}, 
      "pecho": 41.1},
     
-    {"fecha": "02 de Agosto - 2024", 
+    {"fecha": "Agosto - 2024", 
      "peso": 211.6, 
      "brazo": {"izquierdo": 14.1, "derecho": 14.1}, 
      "cintura": {"min": 33.7, "max": 36.4}, 
@@ -69,7 +69,7 @@ datos = [
      "pierna": {"izquierda": 26.1, "derecha": 27}, 
      "pecho": 40},
     
-    {"fecha": "02 de Septiembre - 2024", 
+    {"fecha": "Septiembre - 2024", 
      "peso": 212.6, 
      "brazo": {"izquierdo": 14.3, "derecho": 14.3}, 
      "cintura": {"min": 35, "max": 38}, 
@@ -78,7 +78,7 @@ datos = [
      "pierna": {"izquierda": 27, "derecha": 27}, 
      "pecho": 40},
     
-    {"fecha": "02 de Octubre - 2024", 
+    {"fecha": "Octubre - 2024", 
      "peso": 215, 
      "brazo": {"izquierdo": 15, "derecho": 15}, 
      "cintura": {"min": 36, "max": 37.4}, 
@@ -87,7 +87,7 @@ datos = [
      "pierna": {"izquierda": 27.1, "derecha": 27.3}, 
      "pecho": 42.5},
 
-    {"fecha": "02 de Noviembre - 2024",
+    {"fecha": "Noviembre - 2024",
         "peso": 214.8,
         "brazo": {"izquierdo": 15.7, "derecho": 15.4},
         "cintura": {"min": 35, "max": 38.1},
@@ -96,17 +96,36 @@ datos = [
         "pierna": {"izquierda": 26.4, "derecha": 26.7},
         "pecho": 41},
 
-    {"fecha": "02 de Diciembre - 2024",
+    {"fecha": "Diciembre - 2024",
         "peso": 206.1,
         "brazo": {"izquierdo": 15, "derecho": 14.7},
         "cintura": {"min": 34, "max": 37.7},
         "pliegues": {"min": 20.2, "max": None},
         "pantorrillas": {"izquierda": 16.4, "derecha": 16.4},
         "pierna": {"izquierda": 25.7, "derecha": 25.7},
-        "pecho": 42.3}
+        "pecho": 42.3},
+
+    {"fecha": "Enero - 2025",
+        "peso": 211,
+        "brazo": {"izquierdo": 15.4, "derecho": 15.2},
+        "cintura": {"min": 34, "max": 37.7},
+        "pliegues": {"min": 20.2, "max": None},
+        "pantorrillas": {"izquierda": 16.5, "derecha": 16.3},
+        "pierna": {"izquierda": 27.5, "derecha": 27.1},
+        "pecho": 43.6
+    },
+
+    {"fecha": "Febrero - 2025",
+        "peso": 211.2,
+        "brazo": {"izquierdo": 15.6, "derecho": 15.1},
+        "cintura": {"min": 34.6, "max": 36.7},
+        "pliegues": {"min": 20.1, "max": None},
+        "pantorrillas": {"izquierda": 16.6, "derecha": 16.5},
+        "pierna": {"izquierda": 26, "derecha": 26.3},
+        "pecho": 42.2
+    }
     
 ]
-
 
 # Fechas para el eje x
 fechas = [dato["fecha"] for dato in datos]
